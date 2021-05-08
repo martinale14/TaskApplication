f553b4f162c49220dd22947fa561aea789cbcff0
Martin Alejandro Escobar Espinel <Martinale4@hotmail.com>
2021-05-08 12:53:20 -0500

    Trying to add ios push

---
+++ taskappFlutter/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Martín Alejandro Escobar Espinel on 8/05/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
